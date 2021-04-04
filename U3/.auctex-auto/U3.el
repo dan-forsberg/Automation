e294d26ea279ae22738a137d35525e25db11f908
Dan Örjestad <danorjest@gmail.com>
2021-04-04 16:48:58 +0200

    Städade bort radbrytningar

--- U3/.auctex-auto/U3.el
+++ U3/.auctex-auto/U3.el
@@ -5,13 +5,13 @@
                      '(("article" "11pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -31,17 +31,17 @@
     "capt-of"
     "hyperref")
    (LaTeX-add-labels
-    "sec:org4a95f72"
-    "sec:org97fb23d"
-    "sec:orgf44a8bf"
-    "sec:org9379dfe"
-    "sec:org6c16d08"
-    "sec:orgcd29632"
-    "sec:org54acef2"
-    "sec:orga44b433"
-    "sec:org361d099"
-    "sec:org3e59959"
-    "sec:org2e2e213"
-    "sec:org8786b86"))
+    "sec:orgb7866bd"
+    "sec:org0b94f3f"
+    "sec:org2c52661"
+    "sec:org6de47ba"
+    "sec:org747ca99"
+    "sec:orgfde3186"
+    "sec:orgc1e8f82"
+    "sec:org673a223"
+    "sec:org19b904f"
+    "sec:org95b9703"
+    "sec:org4b5fb94"
+    "sec:org118b702"))
  :latex)
 
