02c8935206a28c09beb87a8cce027be657809cd1
Dan Örjestad <danorjest@gmail.com>
2021-04-05 13:24:09 +0200

    Gjort klart U3

--- U3/.auctex-auto/Forsberg_Dan_U3_1.el
+++ U3/.auctex-auto/Forsberg_Dan_U3_1.el
@@ -1,17 +1,17 @@
 (TeX-add-style-hook
- "U3"
+ "Forsberg_Dan_U3_1"
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("article" "a4paper" "11pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -31,12 +31,12 @@
     "capt-of"
     "hyperref")
    (LaTeX-add-labels
-    "sec:orgeb835f6"
-    "sec:orgd154b5f"
-    "sec:org3bafb61"
-    "sec:orge453a2c"
-    "sec:org6318161"
-    "sec:orgd236a89"
-    "sec:orgb6816aa"))
+    "sec:org7ed468b"
+    "sec:orgb15ceeb"
+    "sec:org2b4d7b0"
+    "sec:orgfb7e90d"
+    "sec:org34a35b2"
+    "sec:org9e06889"
+    "sec:orga147a5a"))
  :latex)
 
