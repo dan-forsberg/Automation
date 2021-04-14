845e5de4b1c422296ad0b173a0309caf46542bb7
Dan Forsberg <dan.s.forsberg@gmail.com>
2021-04-14 08:31:23 +0200

    Fixat U1, börjat fixa U2

---
+++ U1/.auctex-auto/Forsberg_Dan_U1_4.el
@@ -0,0 +1,46 @@
+(TeX-add-style-hook
+ "Forsberg_Dan_U1_4"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "grffile"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "textcomp"
+    "amssymb"
+    "capt-of"
+    "hyperref")
+   (LaTeX-add-labels
+    "sec:org056efff"
+    "sec:orge56a368"
+    "sec:orgd58c449"
+    "sec:org5c947c6"
+    "sec:org6e247dc"
+    "sec:org1621675"
+    "sec:org3e2062b"
+    "sec:org87cfafa"
+    "sec:org19674de"
+    "sec:org36a54e9"
+    "sec:org7578f6a"))
+ :latex)
+
